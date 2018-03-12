3a5ced73716830edfef20aa4bad89810f1095cf2
Mazin Power <muzfuz@users.noreply.github.com>
2018-03-12 16:50:42 +0000

    add parallel_task (#51)

--- rakefile.rb
+++ rakefile.rb
@@ -23,3 +23,47 @@ task :stop_execution_upon_first_testfailure, :test_no, :times do |t, args|
       Rake::Task[:scenario].execute
   }
 end
+
+task :cognito_parallel_tests, [:job_number, :total_jobs] do |_t, args|
+  unless args.job_number || args.total_jobs
+    raise "You must supply a job number and the total number of jobs"
+  end
+
+  job_number = args.job_number.to_i
+  total_jobs = args.total_jobs.to_i
+
+  features_list = CognitoTestList.new(job_number, total_jobs)
+
+  puts "RUNNING JOB NUMBER #{job_number} out of #{total_jobs} total jobs."
+  puts "FEATURES: #{features_list}"
+  puts "EXECUTING WITH: bundle exec cucumber -p cog_e2e $(FEATURES)"
+
+  ok = system("bundle exec cucumber -p cog_e2e #{features_list}")
+  raise("Failed to execute cucumber steps") unless ok
+end
+
+class CognitoTestList
+  def initialize(job_number, total_jobs)
+    @test_list = TestSplitter.new(test_files, total_jobs).chunk_for(job_number)
+  end
+
+  def to_s
+    @test_list
+  end
+
+  def test_files
+    Dir["features/cognito/**/*.feature"]
+  end
+end
+
+class TestSplitter
+  def initialize(test_files, total_jobs)
+    require "parallel_tests/test/runner" # only available in test and development
+    require "parallel_tests/grouper"
+    @test_chunks = ParallelTests::Test::Runner.tests_in_groups(test_files, total_jobs)
+  end
+
+  def chunk_for(job_number)
+    @test_chunks[job_number - 1].join(" ")
+  end
+end
